4f2db18b9a6958cd6d32649b088c408bc1aac55b
utap2001 <36832947+utap2001@users.noreply.github.com>
2026-01-27 23:37:52 +0800

    Create rustdesk.sh

---
+++ rustdesk.sh
@@ -0,0 +1,8 @@
+bash <(wget -qO- https://get.docker.com)
+wget rustdesk.com/oss.yml -O compose.yml
+sudo docker compose up -d
+
+
+#wget https://raw.githubusercontent.com/techahold/rustdeskinstall/master/install.sh
+#chmod +x install.sh
+#./install.sh
