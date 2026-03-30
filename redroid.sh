c436db3187fb7cfbcd3bbb91dbac186505675d63
utap2001 <36832947+utap2001@users.noreply.github.com>
2026-03-31 00:17:19 +0800

    Update redroid.sh

--- redroid.sh
+++ redroid.sh
@@ -2,7 +2,8 @@ sudo apt install linux-modules-extra-`uname -r`
 
 sudo modprobe binder_linux devices="binder,hwbinder,vndbinder"
 
-echo 'binder_linux options binder_linux devices="binder,hwbinder,vndbinder"' | sudo tee -a /etc/modules-load.d/redroid.conf
+echo 'binder_linux' | sudo tee -a /etc/modules-load.d/redroid.conf
+echo 'options binder_linux devices="binder,hwbinder,vndbinder"' | sudo tee -a /etc/modprobe.d/redroid.conf
 
 
 /etc/modules.load.d/binder.conf
